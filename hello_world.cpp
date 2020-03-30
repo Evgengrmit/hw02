59f5af9f642131903111e556bddc5dbb7f85e03b
Evgengrmit <evgengrmit@gmail.com>
2020-03-30 16:52:22 +0300

    Style - Mozilla

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 
-int main(){
-  std::string name;  // Имя пользователя
-  std::cin >> name;  // Ввод имени пользователя
-  std::cout << "Hello world from " << name << std::endl; // Вывод приветствия от пользователя
+int
+main()
+{
+  std::string name; // Имя пользователя
+  std::cin >> name; // Ввод имени пользователя
+  std::cout << "Hello world from " << name
+            << std::endl; // Вывод приветствия от пользователя
   return 0;
 }
